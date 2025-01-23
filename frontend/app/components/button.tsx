698d6a685a2fd8faec86b9c04ef5934c1b86f0de
Zenithao <adrien.le.pober@gmail.com>
2025-01-23 16:59:21 +0100

    add button component

---
+++ frontend/app/components/button.tsx
@@ -0,0 +1,28 @@
+interface Button {
+    text: string;
+    width?: string; 
+    height?: string; 
+    fontSize?: string; 
+    outlined?: boolean; 
+}
+
+export default function Button({ 
+    text,
+    width = 'w-82',
+    height = 'h-12',
+    fontSize = 'text-xl',
+    outlined = false
+}: Button) {
+    const buttonClass = `rounded-[10px] ${width} ${height} ${fontSize} 
+        ${outlined ?
+            'border border-primary hover:bg-primary hover:text-secondary'
+            : 
+            'bg-primary text-secondary'
+        }`;
+
+    return (
+        <button className={buttonClass}>
+            {text}
+        </button>
+    );
+}
