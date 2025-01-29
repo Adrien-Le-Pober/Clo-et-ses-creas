91e71b9f46875079cd75ac4568eb5322d40049f0
Zenithao <adrien.le.pober@gmail.com>
2025-01-29 13:58:51 +0100

    fix loader height

--- frontend/app/components/loader.tsx
+++ frontend/app/components/loader.tsx
@@ -2,7 +2,7 @@ import loader from './../../assets/images/loader.mp4';
 
 export default function Loader() {
     return (
-        <div className="flex justify-center items-center h-16">
+        <div className="flex justify-center items-center min-h-[208px]">
             <video src={loader} loop autoPlay muted className="w-52"></video>
         </div>
     );
