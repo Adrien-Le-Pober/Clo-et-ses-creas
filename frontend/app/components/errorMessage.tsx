9e65c3253ab809fd4d49e535a46e830ec798623f
Zenithao <adrien.le.pober@gmail.com>
2025-02-12 10:28:22 +0100

    add className prop on errorMessage

--- frontend/app/components/errorMessage.tsx
+++ frontend/app/components/errorMessage.tsx
@@ -1,9 +1,12 @@
 interface errorMessageProps {
     message: string;
+    className?: string;
 }
 
-export default function errorMessage({message}: errorMessageProps) {
+export default function errorMessage({message, className}: errorMessageProps) {
+    const classes = `bg-[#F8D7DA] p-2 mb-2 rounded-md ${className}`
+
     return (
-        <p className="bg-[#F8D7DA] p-2 mb-2 rounded-md">{message}</p>
+        <p className={classes}>{message}</p>
     )
 }
