c943129e5fd140a4f4f13acb6f8a009517d2cdbb
Zenithao <adrien.le.pober@gmail.com>
2025-03-24 09:36:05 +0100

    expose product variant weight on api

--- backend/src/Entity/Product/ProductVariant.php
+++ backend/src/Entity/Product/ProductVariant.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Entity\Product;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Sylius\Component\Core\Model\ProductVariant as BaseProductVariant;
 use Sylius\Component\Product\Model\ProductVariantTranslationInterface;
 
@@ -12,6 +13,12 @@ use Sylius\Component\Product\Model\ProductVariantTranslationInterface;
 #[ORM\Table(name: 'sylius_product_variant')]
 class ProductVariant extends BaseProductVariant
 {
+    #[Groups(["sylius:shop:product_variant:show", "sylius:shop:cart:show"])]
+    public function getWeight(): ?float
+    {
+        return parent::getWeight();
+    }
+
     protected function createTranslation(): ProductVariantTranslationInterface
     {
         return new ProductVariantTranslation();
