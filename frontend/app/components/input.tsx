b9fbad81af67653ec0925b4a12fe7f4e6a0c53d5
Zenithao <adrien.le.pober@gmail.com>
2025-12-04 18:37:27 +0100

    add textarea option on input component

--- frontend/app/components/input.tsx
+++ frontend/app/components/input.tsx
@@ -9,6 +9,7 @@ interface InputProps {
     customClasses?: string;
     errorMsg?: string;
     register?: UseFormRegisterReturn;
+    textarea?: boolean;
 }
 
 export default function Input({
@@ -19,24 +20,32 @@ export default function Input({
     customClasses,
     errorMsg,
     register,
+    textarea = false,
 }: InputProps) {
     const inputClass=`border rounded-lg px-2 focus:outline-primary ${width} ${customClasses}`
 
     return (
-        <>
-            <div className="flex flex-col pb-7">
-                <label htmlFor={name} className="pb-3 md:pb-5">{label}</label>
+        <div className="flex flex-col pb-7">
+            <label htmlFor={name} className="pb-3 md:pb-5">{label}</label>
+
+            {textarea ? (
+                <textarea
+                    name={name}
+                    className={inputClass}
+                    {...register}
+                />
+            ) : (
                 <input
                     type={type}
                     name={name}
                     className={inputClass}
                     {...register}
                 />
+            )}
 
-                {errorMsg && (
-                    <ErrorMessage message={errorMsg}/>
-                )}
-            </div>
-        </>
+            {errorMsg && (
+                <ErrorMessage message={errorMsg}/>
+            )}
+        </div>
     );
 }
