8804b30f39f9c57e222db0a2e4c8c180df2bccda
Zenithao <adrien.le.pober@gmail.com>
2025-12-16 23:02:12 +0100

    add my account page

--- frontend/app/my-account/myAccountPage.tsx
+++ frontend/app/my-account/myAccountPage.tsx
@@ -1,9 +1,25 @@
+import Accordion from "~/components/accordion";
+import AccountPersonalInfoForm from "~/components/accountPersonalInfoForm";
+
 export default function MyAccountPage() {
     return (
-        <>
-            <div className="flex justify-center  md:justify-start">
-                <h1 className="pb-28 pt-20 md:pb-24 md:pt-32 text-4xl">mon compte</h1>
+        <div className="flex flex-col items-center px-4">
+
+            <h1 className="pb-10 pt-20 md:pt-32 text-4xl">
+                Mon compte
+            </h1>
+
+            <div className="w-full max-w-xl"> 
+                <Accordion title="Informations personnelles" defaultOpen>
+                    <AccountPersonalInfoForm />
+                </Accordion>
+
+                <Accordion title="Modifier mon mot de passe">
+                    "{/* Formulaire à venir */}"
+                </Accordion>
+
+                "{/* Bouton + modal confirmation */}"
             </div>
-        </>
+        </div>
     );
 }
